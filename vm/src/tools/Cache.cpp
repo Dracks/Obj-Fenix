3f34210d1b67d1bfba9c7c82b3338bcb8c3c5e6b
Dracks <dracks@Eickot-MBP.(none)>
2011-09-29 11:20:14 +0200

    Modificacions al canviar a linux

--- vm/src/tools/Cache.cpp
+++ vm/src/tools/Cache.cpp
@@ -7,25 +7,45 @@
 //
 
 #include <iostream>
+#include <vector>
+#include "Cache.h"
 
 
 namespace tools {
 //	class CacheClass {
-	CacheClass::CacheClass(int, string);
-	CacheClass::~CacheClass();
-		void CacheClass::apply(Adapter*)=0;
-		string CacheClass::getName();
-		int CacheClass::getUID();
+	CacheClass::CacheClass(int, string){
+
+        }
+
+	CacheClass::~CacheClass(){}
+
+        string CacheClass::getName(){
+            return this->name;
+
+        }
+        int CacheClass::getUID(){
+            return this->ID;
+        }
 		
-		void CacheClass::addProperty(int uid, string name);
-		vector<pair<int, string> > CacheClass::getProperties();
+        void CacheClass::addProperty(int uid, string name){
+            this->properties.push_back(new pair<int, string>(uid, name));
+        }
+
+        vector<pair<int, string> > CacheClass::getProperties(){
+            return this->properties;
+        }
 	
 //	class NativeCacheClass: public CacheClass {
 	vector<pair<int, string> > NativeCacheClass::getListMethod(){
+            return this->methods;
 	}
-	void NativeCacheClass::addMethod(int, string){}
+	void NativeCacheClass::addMethod(int id, string name){
+            this->methods.push_back(new pair(id, name));
+        }
 		
-	void NativeCacheClass::apply(Adapter*){}
+	void NativeCacheClass::apply(Adapter* obj){
+            obj->applyNative(this);
+        }
 	
 //	class OfxCacheMethod {
 		
